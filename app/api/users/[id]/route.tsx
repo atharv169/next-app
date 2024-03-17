6def1956eddebbd698adf3d7856aa216ba0bb4ec
atharv169 <119473550+atharv169@users.noreply.github.com>
2024-03-17 10:58:24 +0530

    delete api

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -27,5 +27,13 @@ export async function PUT(request:NextRequest,
     return NextResponse.json({id:1,name:body.name})
     //if doesnt exist,return
     // Update the user return the updated user
+}
+
+export async function DELETE(request:NextRequest, 
+    {params}:{params:{id:number}}){
+        if(params.id>10)
+         return NextResponse.json({error:'user not found'},{status:404})
+
+         return NextResponse.json({});
 
 }
