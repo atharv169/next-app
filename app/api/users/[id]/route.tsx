0b965b73ed009047f29a0070f6920d53f02e6bdb
atharv169 <119473550+atharv169@users.noreply.github.com>
2024-03-17 10:52:46 +0530

    update api

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,22 @@ export function GET(request:NextRequest,
     
     return NextResponse.json({id:1,name:'Atharv'})
 }
+// PUT-replacing object
+// PATCH to update one or more properties
+
+export async function PUT(request:NextRequest, 
+    {params}:{params:{id:number}}){
+    //validate the request body
+    const body=await request.json();
+    //if invalid,return 400  
+    if(!body.name)
+    return NextResponse.json({error:'Name is required'},{status:400})
+    //fetch the user with the given id
+    if(params.id>10)
+    return NextResponse.json({error:'user is not found'},{status:404})
+
+    return NextResponse.json({id:1,name:body.name})
+    //if doesnt exist,return
+    // Update the user return the updated user
+
+}
