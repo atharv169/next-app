fbbaf98d0b41abfc950cfaecf6d56fed06c7bde7
atharv169 <119473550+atharv169@users.noreply.github.com>
2024-03-24 15:18:39 +0530

    styling email

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,30 @@
+import React, { CSSProperties } from 'react'
+import {Html,Body,Container,Text,Link,Preview,Tailwind,} from '@react-email/components'
+
+const WelcomeTemplate = ({name}:{name:string}) => {
+  return (
+    <Html>
+        <Preview>Welcome aboard!</Preview>
+        <Tailwind>
+        {/* <Body style={body}> */}
+        <Body className='bg-white'>
+            <Container>
+                {/* <Text style={heading}>Hello {name}</Text> */}
+                <Text className='font-bold text-3xl'>Hello {name}</Text>
+                <Link href='https://google.com'>www.google.com</Link>
+            </Container>
+        </Body>
+        </Tailwind>
+    </Html>
+  )
+};
+
+// const body:CSSProperties={
+//     background:'#fff'
+// }
+
+// const heading:CSSProperties={
+//     fontSize:'32px'
+// }
+
+export default WelcomeTemplate
